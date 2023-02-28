994d877db7a0cc1d9bffb26812b896d429f1c591
hgrandjean <121041961+hgrandjean@users.noreply.github.com>
2023-02-28 23:19:31 +0100

    Update split.py

--- split.py
+++ split.py
@@ -1,25 +1,27 @@
-with open("kmr_temp/A2T3P0_NSP2_ROTSH.kmr" , "r") as pos : 
+import math 
+with open("kmr_temp/sp0.kmr" , "r") as pos : 
     positif = list (pos.readlines()) #replace set by something else 
-with open("kmr_temp/Q9FI78_HST_ARATH.kmr" , "r") as neg : 
+with open("kmr_temp/sp1.kmr" , "r") as neg : 
     negatif = list (neg.readlines()) #replace set by something else 
 
-#count of descriptors in positive list 
+#count of descriptors in positive then in negatif list 
 kmers_counter={}
 for kmer in positif:
     if kmer in kmers_counter.keys() : 
       kmers_counter[kmer][0] +=1 
     else : 
-      kmers_counter[kmer] = [1,0]
+      kmers_counter[kmer] = [1,0,0]
 for kmer in negatif:
     if kmer in kmers_counter.keys() : 
       kmers_counter[kmer][1] +=1 
     else : 
-      kmers_counter[kmer] = [0,1]
-          
-#count of descriptors in negatif list 
+      kmers_counter[kmer] = [0,1, 0]
+
+#score attribution to each descriptor
+for kmer in kmers_counter.keys() : 
+    kmers_counter[kmer][2] = math.log((kmers_counter[kmer][0]+1)/(kmers_counter[kmer][1]+1))
 
 
 with open("unique_set.tsv" , "w") as save : 
     for kmer in kmers_counter.keys() : 
-      save.write(kmer+'\t'+str(kmers_counter[kmer])+'\n')
-      
+      save.write(str(kmer).strip()+'\t'+str(kmers_counter[kmer])+'\n') 
