1b81bfa03a826f8886a951f8e4bdc2e85e72d7be
hgrandjean <121041961+hgrandjean@users.noreply.github.com>
2023-03-12 02:27:30 +0100

    peptide scoring from descriptors score

---
+++ score_peptide.py
@@ -0,0 +1,28 @@
+from kmer_parser import reduce_seq , gap_kmer , find_kmer
+
+def score_kmers(pep_seq , reduce, score_dict ):
+      kmer_score = 0
+      size = min(len(pep_seq), 5)
+      if size <= 2 : gap = 0 
+      else : gap = size - 2 
+      kmers = find_kmer (sequence = pep_seq , kmer_size = size , ngap = gap , reduce = reduce )
+      for kmer in kmers:
+          if kmer in score_dict.keys() : 
+              kmer_score += score_dict[kmer][2]
+              # print(kmer," ", score_dict[kmer][2])
+      return kmer_score
+    
+    
+
+score_dict = {}
+
+with open("unique_set.tsv", "r" ) as scores :
+    for line in scores:
+        key, value = line.removesuffix('\n').split('\t')
+        value =  value.strip('][').split(', ')
+        value = [float(x) for x in value ]
+        score_dict[key] = value 
+
+
+score = score_kmers("RGLRRLGRKIAHGVKKYG ",6,score_dict) # SMAP-18
+print(score)
