9726543ee99e7acede372748f249c88f2e10a235
Hugo Grandjean <121041961+hgrandjean@users.noreply.github.com>
2023-04-03 10:14:18 +0200

    commenting code and adding control points

--- score_peptide.py
+++ score_peptide.py
@@ -1,6 +1,15 @@
+### import dependencies ####
 from kmer_parser import reduce_seq , gap_kmer , find_kmer
 
+
+### scoring each descriptor found in the given peptide using score previously computed ###
+### uses find_kmer function from kmer_parser ###
 def score_kmers(pep_seq , reduce, score_dict ):
+      '''
+      pep_seq : str of peptide sequence
+      reduce : bool precise reduction of the amino acid dictionnary (20 AA) to a specific one (see RED dictionnaries) 
+      score_dict : dict loadded scores and kmer sequences 
+      '''
       kmer_score = 0
       size = min(len(pep_seq), 5)
       if size <= 2 : gap = 0 
@@ -26,5 +35,5 @@ with open(score_file, "r" ) as scores :
         score_dict[key] = value 
 print ("Finished loading scores \n Starting scoring peptides")
 
-score = score_kmers("RGLRRLGRKIAHGVKKYG ",6,score_dict) # SMAP-18
+score = score_kmers("RGLRRLGRKIAHGVKKYG ",6,score_dict) # SMAP-18 peptide in RED6 reduction dictionnary 
 print(score)
