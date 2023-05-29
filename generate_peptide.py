67681a22c7b4b5e25304205d657cfcea7fd7a3b4
Magali De Carvalho <decarvalho.magali@gmail.com>
2023-05-29 16:28:17 +0200

    refacto(generate_peptide): main function and use rich for progress bar

--- generate_peptide.py
+++ generate_peptide.py
@@ -12,6 +12,10 @@ from scipy.signal import find_peaks
 
 from kmer_parser import find_kmer
 
+from rich import print as rich_print
+from rich.panel import Panel
+from rich.progress import BarColumn, Progress, SpinnerColumn, MofNCompleteColumn, TaskID, TextColumn, TimeElapsedColumn
+
 print(
     "                                                                                                                                                       "
 )
@@ -124,25 +128,23 @@ print(
     "\n \n \n #######################################################################################################################################################"
 )
 
-# Selected reduction dictionary
-reduce = 6
+SCORE_FILE = "results/descriptors_activity_scores.tsv"
+PAM2_EXCEL_FILE = "resources/PAM_2_substitution_probabilities_formated.xlsx"
+AA_ORDER = "ARNDCQEGHILKMFPSTWYV"
+DEFAULT_PEPTIDE = "RGLRRLGRKIAHGVKKYG"
+NB_PEPTIDE = 50
+NB_ITERATIONS = 1000
 
-def progress_bar(count, total, size=100, sides="[]", full="#", empty=".", prefix=""):
-    x = int(size * count / total)
-    sys.stdout.write(
-        "\r"
-        + prefix
-        + sides[0]
-        + full * x
-        + empty * (size - x)
-        + sides[1]
-        + " "
-        + str(count).rjust(len(str(total)), " ")
-        + "/"
-        + str(total)
+
+def default_progress():
+    return Progress(
+        SpinnerColumn(),
+        TimeElapsedColumn(),
+        BarColumn(),
+        MofNCompleteColumn(),
+        TextColumn("[bold blue]({task.description})"),
+        transient=False,
     )
-    if count == total:
-        sys.stdout.write("\n")
 
 
 ### scoring each descriptor found in the given peptide using score previously computed ###
@@ -168,21 +170,16 @@ def score_kmers(pep_seq, reduce, score_dict):
     return kmer_score / len(pep_seq)
 
 
-score_file = "results/descriptors_activity_scores.tsv"
-### loading score from computed tsv file ###
-
-print(f"Loading descriptors scores from file : {score_file}")
-score_dict = {}
-with open(score_file, "r") as scores:
-    for line in scores:
-        key, value = line.removesuffix("\n").split("\t")
-        value = value.strip("][").split(", ")
-        value = [float(x) for x in value]
-        score_dict[key] = value
-print("Finished loading scores")
-print(
-    "####################################################################################################################################################### \n \n \n"
-)
+def load_descriptors_score(score_file: str = SCORE_FILE) -> dict:
+    rich_print(Panel(f"Loading descriptors scores from file : {score_file}", style="light_slate_blue"))
+    score_dict = {}
+    with open(score_file, "r") as scores:
+        for line in scores:
+            key, value = line.removesuffix("\n").split("\t")
+            value = value.strip("][").split(", ")
+            value = [float(x) for x in value]
+            score_dict[key] = value
+    return score_dict
 
 
 ### computation of peptide physical properties ###
@@ -199,6 +196,100 @@ def pep_physical_analysis(peptide):
     peaks, _ = find_peaks(hydropho, distance=2)  # identify maximums miniaml distance between two peaks = 2
     space = np.mean(np.diff(peaks))  # mean space between hydrophilic maximums
     return [peptide, helix_prob * 100, charge, space]
+
+
+def generate_prob_dict_from_excel(file_name: str = PAM2_EXCEL_FILE) -> dict:
+    pam2_prob_matrix = pd.read_excel(file_name)
+    print(f"Loading PAM substitution probabilities")
+    print(f"Finished loading PAM substitution probabilities \n \n \n")
+
+    # Convert the DataFrame to a dictionary
+    pam2_prob_dict = {}
+    for row in pam2_prob_matrix.to_numpy():
+        pam2_prob_dict[row[0]] = row[1:]
+    return pam2_prob_dict
+
+
+def generate_peptides(
+    aa_probs: dict, nb_peptide: int = NB_PEPTIDE, peptide: str = DEFAULT_PEPTIDE, bootstrap: int = NB_ITERATIONS
+):
+    # Test
+    score_evolution = []
+    helix_proba_evol = []
+    charge_evol = []
+    space_evolution = []
+    peptides_generated = []
+
+    for p in range(0, nb_peptide):
+        with default_progress() as progress:
+            task_id: TaskID = progress.add_task(f"Generating peptide number {p+1} out of {nb_peptide}", total=bootstrap)
+            for i in range(bootstrap):
+                # randomisation of mutation location in the peptide sequence should be applied to biological form (To develop)
+                random_index = random.randint(0, len(peptide) - 1)
+
+                # replacing the amino acid selected to a knew one
+                random_amino_acid = peptide[random_index]
+                prob = aa_probs[random_amino_acid]
+                new_amino_acid = random.choices(AA_ORDER, prob, k=1)[0]
+                new_peptide = peptide[:random_index] + new_amino_acid + peptide[random_index + 1 :]
+
+                # Calculating scores of previous and new peptides sequences
+                peptide_score = score_kmers(peptide, 6, score_dict)
+                # score_evolution.append(peptide_score)
+
+                physical_analysis = pep_physical_analysis(peptide)
+                # helix_proba_evol.append(physical_analysis[1])
+                # charge_evol.append(physical_analysis[2])
+                # space_evolution.append(physical_analysis[3])
+
+                new_peptide_score = score_kmers(new_peptide, 6, score_dict)
+
+                # The peptide is selected if new score is higher
+                score_difference = new_peptide_score - peptide_score
+
+                if score_difference > 0:
+                    peptide = new_peptide
+
+                # addtion of randomness : if the mutation is not too much unfavored by the env then it can appen to be selected (To develop)
+
+                # else:
+                #   probability_of_acceptance = 1**(score_difference/100000)
+                #    if random.random() < probability_of_acceptance:
+                #        peptide = new_peptide
+
+                # progress bar HERE
+                progress.update(task_id, advance=1)
+
+        rich_print("\nFinal peptide : \n")
+        rich_print(peptide)
+        rich_print(f"final score : {score_kmers(peptide,6,score_dict)}")
+        rich_print(f"final helix probability : {round(physical_analysis[1], 2)} %")
+        rich_print(f"final global charge Q : {physical_analysis[2]}")
+        rich_print(f"final hydrophobicity frequency : {physical_analysis[3]}")
+        rich_print(f"{p+1} sequence generated over {nb_peptide}\n ")
+        score_evolution.append(new_peptide_score)
+
+        helix_proba_evol.append(physical_analysis[1])
+        charge_evol.append(physical_analysis[2])
+        space_evolution.append(physical_analysis[3])
+        peptides_generated.append(peptide)
+    print(score_evolution)
+    final_df = pd.DataFrame(
+        list(zip(peptides_generated, score_evolution, charge_evol, space_evolution)),
+        columns=["peptide_sequence", "score", "charge", "hydro_frequency"],
+    )
+    print(final_df)
+
+
+if __name__ == "__main__":
+    # Getting scores from CSV file
+    score_dict = load_descriptors_score()
+
+    # Import substitution probabilities from PAM2 data frame relative to mutation frequencies with conservation excluded
+    pam2_probs = generate_prob_dict_from_excel()
+
+    ### generation and optimisation of a peptide sequence
+    generate_peptides(pam2_probs)
 
 
 """
@@ -228,18 +319,6 @@ print(data)
 data.to_excel("computed_db.xlsx")
 """
 
-# import substitution probabilities from PAM2 data frame relative to mutation frequencies with conservation excluded
-pam2_prob_matrix = pd.read_excel("resources/PAM_2_substitution_probabilities_formated.xlsx")
-print(f"Loading PAM substitution probabilities")
-aa_order = "ARNDCQEGHILKMFPSTWYV"
-print(f"Finished loading PAM substitution probabilities \n \n \n")
-
-# Convert the Dataframe into a dictionary
-pam2_prob_dict: dict = {}
-for row in pam2_prob_matrix.to_numpy():
-    pam2_prob_dict[row[0]] = row[1:]
-
-
 """ Clustering -> not succesful
 ### visualisation of multidimensionnal data ###
 fig=plt.figure()
@@ -251,88 +330,6 @@ ax.set_ylabel("Rel IC50")
 ax.set_zlabel("helix_prob")
 plt.show()
 """
-
-### generation and optimisation of a peptide sequence
-print(
-    "####################################################################################################################################################### \n \n \n"
-)
-npep = 50
-peptide = "RGLRRLGRKIAHGVKKYG"
-score_kmers(peptide, 6, score_dict)
-bootstrap_iterations = 1000
-score_evolution = []
-helix_proba_evol = []
-charge_evol = []
-space_evolution = []
-peptides_generated = []
-bootstrap = range(bootstrap_iterations)
-print("Starting generation of {npep}peptides from {bootstrap_iterations} bootstrap iterations \n ")
-
-# Test
-for p in range(0, npep):
-    peptide = "RGLRRLGRKIAHGVKKYG"
-    for i in bootstrap:
-        # randomisation of mutation location in the peptide sequence should be applied to biological form (To develop)
-        random_index = random.randint(0, len(peptide) - 1)
-
-        # replacing the amino acid selected to a knew one
-        random_amino_acid = peptide[random_index]
-        prob = pam2_prob_dict[random_amino_acid]
-        new_amino_acid = random.choices(aa_order)[0]
-        new_peptide = peptide[:random_index] + new_amino_acid + peptide[random_index + 1 :]
-
-        # Calculating scores of previous and new peptides sequences
-        peptide_score = score_kmers(peptide, 6, score_dict)
-        # score_evolution.append(peptide_score)
-
-        physical_analysis = pep_physical_analysis(peptide)
-        # helix_proba_evol.append(physical_analysis[1])
-        # charge_evol.append(physical_analysis[2])
-        # space_evolution.append(physical_analysis[3])
-
-        new_peptide_score = score_kmers(new_peptide, 6, score_dict)
-
-        # The peptide is selected if new score is higher
-        score_difference = new_peptide_score - peptide_score
-
-        if score_difference > 0:
-            peptide = new_peptide
-
-        # addtion of randomness : if the mutation is not too much unfavored by the env then it can appen to be selected (To develop)
-
-        # else:
-        #   probability_of_acceptance = 1**(score_difference/100000)
-        #    if random.random() < probability_of_acceptance:
-        #        peptide = new_peptide
-        progress_bar(
-            count=i + 1,
-            total=bootstrap_iterations,
-            size=100,
-            sides="||",
-            full="#",
-            empty=" ",
-            prefix="Performing bootstraps... ",
-        )
-
-    print("\nFinal peptide : \n")
-    print(peptide)
-    print(f"final score : {score_kmers(peptide,6,score_dict)}")
-    print(f"final helix probability : {round(physical_analysis[1], 2)} %")
-    print(f"final global charge Q : {physical_analysis[2]}")
-    print(f"final hydrophobicity frequency : {physical_analysis[3]}")
-    print(f"{p+1} sequence generated over {npep} \n \n ")
-    score_evolution.append(new_peptide_score)
-
-    helix_proba_evol.append(physical_analysis[1])
-    charge_evol.append(physical_analysis[2])
-    space_evolution.append(physical_analysis[3])
-    peptides_generated.append(peptide)
-print(score_evolution)
-final_df = pd.DataFrame(
-    list(zip(peptides_generated, score_evolution, charge_evol, space_evolution)),
-    columns=["peptide_sequence", "score", "charge", "hydro_frequency"],
-)
-print(final_df)
 
 # plt.plot(bootstrap, score_evolution ,label = "score")
 # plt.plot(bootstrap, helix_proba_evol ,label = "helix probability")
