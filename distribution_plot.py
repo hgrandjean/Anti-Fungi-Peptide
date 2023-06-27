ea3c382445c11e5d6dd29742c84ded1a8c5c6e42
Ekaterina Kukhtenko <wandererfromsiberia@yandex.ru>
2023-06-27 22:33:58 +0200

    refacto: distribution_plot.py

--- distribution_plot.py
+++ distribution_plot.py
@@ -1,41 +1,35 @@
 from kmer_parser import parse_fasta_file
-from generate_peptide import score_kmers
+from generate_peptide import score_kmers, load_descriptors_score
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from Bio import SeqIO
 
-peptides = pd.read_excel('results/de_novo_peptide_library.xlsx') #for excel files
-file_name = "resources/filtered_positive_db.fasta" #for fasta files
+# Selected reduction dictionary
+REDUCE = 6
 
-reduce = 6
+PEPTIDES = pd.read_excel('results/de_novo_peptide_library.xlsx') # For Excel files
+POSITIVE_DB = "resources/filtered_positive_db.fasta"
+
+# Loading score from computed .tsv file
+SCORE_FILE = 'results/descriptors_activity_scores.tsv'
 
 '''
 Scoring of each descriptor found in the given peptide using previously computed scores. 
 Uses find_kmer function from kmer_parser
 '''
 
-# Loading score from computed .tsv file
-score_file = 'results/descriptors_activity_scores.tsv'
-
-print (f"Loading descriptors scores from file: {score_file}")
-score_dict: dict = {}
-with open(score_file, "r" ) as scores:
-    for line in scores:
-        key, value = line.removesuffix('\n').split('\t')
-        value =  value.strip('][').split(', ')
-        value = [float(x) for x in value]
-        score_dict[key] = value
+print (f"Loading descriptors scores from file: {SCORE_FILE}")
+score_dictionary = load_descriptors_score(SCORE_FILE)
 print ("Finished loading scores")
 print('####################################################################################################################################################### \n \n \n')
 
-scores = []
+scores_DB = []
 
 # For .fasta files:
-multi_fasta = parse_fasta_file (file_name)
+multi_fasta = parse_fasta_file (POSITIVE_DB)
 for fasta in multi_fasta:
     seq_db = fasta.seq
-    scores.append(score_kmers(seq_db, r_dict = reduce, score_dictionary = score_dict))
+    scores_DB.append(score_kmers(seq_db, r_dict = REDUCE, score_dict = score_dictionary))
 
 sns.histplot(scores, bins = 20, kde = True).set(title = "Distribution of peptide scores from positive DB", xlabel = "Scores", ylabel = "Count")
 plt.ylim(0, 1)
